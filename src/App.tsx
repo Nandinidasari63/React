b3d6a7087451671c4d4d8de4bef512ecc53b5889
Dasari Nandini <nandinidasari728@gmail.com>
2026-04-20 18:12:54 +0530

    migrates todo state management to useReducer
    * Replaces useState with useReducer in App
    * Introduces reducer function for handling actions
    * Adds action types: add-task, delete-task, toggle-done
    * Removes individual handler functions (addTask, toggleTask, deleteTask)
    *  Passes dispatch instead of multiple callbacks to TodoList
    * Simplifies component logic and centralize state updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 type Task = {
   id: number;
@@ -13,14 +13,44 @@ type Todo = {
 
 type TodoListProps = {
   todoList: Todo;
-  setTodoList: (x: Todo) => void;
-  addTask: (x: number, y: string) => Todo;
-  toggleTask: (x: number) => Todo;
-  onDelete: (x: number) => Todo;
+  dispatch: React.Dispatch<Action>;
+};
+
+type Action =
+  | { type: "add-task"; title: string }
+  | { type: "delete-task"; taskId: number }
+  | { type: "toggle-done"; taskId: number };
+
+const reducer = (todoList: Todo, action: Action) => {
+  switch (action.type) {
+    case "add-task":
+      return {
+        nextId: todoList.nextId + 1,
+        tasks: [...todoList.tasks, {
+          id: todoList.nextId,
+          title: action.title,
+          isDone: false,
+        }],
+      };
+    case "delete-task":
+      return {
+        nextId: todoList.nextId + 1,
+        tasks: todoList.tasks.filter((task) => task.id !== action.taskId),
+      };
+
+    case "toggle-done":
+      return {
+        nextId: todoList.nextId + 1,
+        tasks: todoList.tasks.map((task) =>
+          task.id !== action.taskId ? task : { ...task, isDone: !task.isDone }
+        ),
+      };
+  }
+  return todoList;
 };
 
 const TodoList = (
-  { todoList, setTodoList, addTask, toggleTask, onDelete }: TodoListProps,
+  { todoList, dispatch }: TodoListProps,
 ) => {
   const [text, setText] = useState("");
 
@@ -32,8 +62,7 @@ const TodoList = (
             key={id}
             onClick={(e) => {
               e.stopPropagation();
-              const state = toggleTask(id);
-              setTodoList(state);
+              dispatch({ type: "toggle-done", taskId: id });
             }}
           >
             {id} : {title}
@@ -42,8 +71,7 @@ const TodoList = (
               type="button"
               onClick={(e) => {
                 e.stopPropagation();
-                const state = onDelete(id);
-                setTodoList(state);
+                dispatch({ type: "delete-task", taskId: id });
               }}
             >
               delete
@@ -54,8 +82,7 @@ const TodoList = (
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          const state = addTask(todoList.nextId, text);
-          setTodoList(state);
+          dispatch({ type: "add-task", title: text });
           setText("");
         }}
       >
@@ -71,39 +98,16 @@ const TodoList = (
 };
 
 const App = () => {
-  const [todoList, setTodoList] = useState<Todo>({ nextId: 1, tasks: [] });
-
-  const addTask = (id: number, title: string): Todo => {
-    return {
-      nextId: todoList.nextId + 1,
-      tasks: [...todoList.tasks, { id, title, isDone: false }],
-    };
-  };
-
-  const toggleTask = (taskId: number): Todo => {
-    return {
-      nextId: todoList.nextId + 1,
-      tasks: todoList.tasks.map((task) =>
-        task.id !== taskId ? task : { ...task, isDone: !task.isDone }
-      ),
-    };
-  };
-
-  const deleteTask = (taskId: number): Todo => {
-    return {
-      nextId: todoList.nextId + 1,
-      tasks: todoList.tasks.filter((task) => task.id !== taskId),
-    };
-  };
-
+  // const [todoList, setTodoList] = useState<Todo>({ nextId: 1, tasks: [] });
+  const [todoList, dispatch] = useReducer(reducer, {
+    nextId: 1,
+    tasks: [],
+  });
   return (
     <>
       <TodoList
         todoList={todoList}
-        setTodoList={setTodoList}
-        addTask={addTask}
-        toggleTask={toggleTask}
-        onDelete={deleteTask}
+        dispatch={dispatch}
       />
     </>
   );
